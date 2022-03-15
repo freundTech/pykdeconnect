88ac2af0c94dd8b849e28f91f6ad3bfb477d0540
Adrian Freund <adrian@freund.io>
2022-03-15 09:23:36 +0100

    Don't allow passing None to is_plugin_compatible

--- pykdeconnect/plugin_registry.py
+++ pykdeconnect/plugin_registry.py
@@ -91,14 +91,7 @@ class PluginRegistry:
 
         return self.get_plugin(device, plugin_class)
 
-    def is_plugin_compatible(
-            self,
-            device: KdeConnectDevice,
-            plugin_class: Type[Plugin] | None
-    ) -> bool:
-        if plugin_class is None:
-            return True
-
+    def is_plugin_compatible(self, device: KdeConnectDevice, plugin_class: Type[Plugin]) -> bool:
         try:
             self._check_plugin_compatibility(device, plugin_class)
         except IncompatiblePluginError:
